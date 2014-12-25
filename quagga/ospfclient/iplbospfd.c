c12754682e069ed955466fa4ada3abf51e750eb4
Ryo Nakamura <upa@haeena.net>
2014-12-25 20:59:37 +0900

    add ospf_lsdb related

--- quagga/ospfclient/iplbospfd.c
+++ quagga/ospfclient/iplbospfd.c
@@ -6,14 +6,25 @@
 #include <zebra.h>
 #include "prefix.h"	/* needed by ospf_asbr.h */
 #include "privs.h"
+#include "table.h"
 #include "log.h"
 
 #include "ospfd/ospfd.h"
 #include "ospfd/ospf_asbr.h"
 #include "ospfd/ospf_lsa.h"
+#include "ospfd/ospf_lsdb.h"
 #include "ospfd/ospf_opaque.h"
 #include "ospfd/ospf_api.h"
 #include "ospf_apiclient.h"
+
+/* copied from nm_util.h */
+#define D(format, ...)                                  \
+        fprintf(stderr, "%s [%d] " format "\n",         \
+                __FUNCTION__, __LINE__, ##__VA_ARGS__)
+
+#define ASYNCPORT	4000
+
+
 
 /* privilages struct.
  */
@@ -30,14 +41,63 @@ struct zebra_privs_t ospfd_privs = {
 #include "log.h"
 
 
-#define ASYNCPORT	4000
+
+
+
+
 
 struct thread_master * master;
 struct ospf_apiclient * oc;
+struct ospf_lsdb * lsdb;
+
+
+static struct ospf_lsa *
+ospf_lsa_new_from_header (struct lsa_header * lsah)
+{
+	struct ospf_lsa * new;
+
+	new = ospf_lsa_new ();
+	
+	new->data = ospf_lsa_data_new (lsah->length);
+	memcpy (new->data, lsah, lsah->length);
+
+	return new;
+}
+
+
+static void
+ospf_lsdb_dump (struct ospf_lsdb * db)
+{
+	int type;
+	struct ospf_lsa * lsa;
+	struct route_node * rn;
+	struct route_table * rt;
+
+
+
+	printf ("================ Dump LSDB ================\n");
+
+	for (type = 0; type < OSPF_MAX_LSA; type++) {
+
+		if ((rt = db->type[type].db) == NULL)
+			continue;
+
+
+		for (rn = route_top (rt); rn; rn = route_next (rn)) {
+			if ((lsa = rn->info) != NULL) {
+				ospf_lsa_header_dump (lsa->data);
+			}
+		}
+	}
+
+	printf ("===========================================\n\n\n");	
+
+	return;
+}
 
 
 static int
-lsa_read (struct thread * thread)
+iplbospfd_lsa_read (struct thread * thread)
 {
 	int fd;
 	int ret;
@@ -48,7 +108,7 @@ lsa_read (struct thread * thread)
 
 	ret = ospf_apiclient_handle_async (oc);	// do callback functions.
 	if (ret < 0) {
-		printf ("%s: ospf_apiclient_handle_async failed\n", __func__);
+		D ("%s: ospf_apiclient_handle_async failed\n", __func__);
 		exit (1);
 	}
 
@@ -57,14 +117,14 @@ lsa_read (struct thread * thread)
 	x[0].events = POLLIN;
 	if (poll (x, 1, 0) == 0) {
 		/* no LSA message in the fd. re-compute LSDB ! */
-		printf ("re-compute LSDB !!\n");
+		D ("re-compute LSDB !!");
+		ospf_lsdb_dump (lsdb);
 	}
 
-	thread_add_read (master, lsa_read, oc, fd);
+	thread_add_read (master, iplbospfd_lsa_read, oc, fd);
 
 	return 0;
 }
-
 
 /*
  * Callback functions for asyncronous events.
@@ -72,24 +132,36 @@ lsa_read (struct thread * thread)
 
 static void
 lsa_update_callback (struct in_addr ifaddr, struct in_addr area_id,
-		     u_char is_self_originated, struct lsa_header * lsa)
+		     u_char is_self_originated, struct lsa_header * lsah)
 {
 	/* update LSDB, and calculate relay points, and re-install it */
-	printf ("%s\n", __func__);
 
-	ospf_lsa_header_dump (lsa);
+	struct ospf_lsa * lsa;
+
+	lsa = ospf_lsa_new_from_header (lsah);
+	ospf_lsdb_add (lsdb, lsa);
 
 	return;
 }
 
 static void
 lsa_delete_callback (struct in_addr ifaddr, struct in_addr area_id,
-		     u_char is_self_originated, struct lsa_header * lsa)
+		     u_char is_self_originated, struct lsa_header * lsah)
 {
 	/* update LSDB, and calculate relay points, and re-install it */
-	printf ("%s\n", __func__);
 
-	ospf_lsa_header_dump (lsa);
+	struct ospf_lsa * lsa, * old;
+
+	lsa = ospf_lsa_new_from_header (lsah);
+
+	old = ospf_lsdb_lookup (lsdb, lsa);
+	if (old)
+		ospf_lsdb_delete (lsdb, old);
+	else 
+		D ("Old LSA is not found in LSDB!!");
+
+	lsa->lock--;
+	ospf_lsa_free (lsa);
 
 	return;
 }
@@ -115,6 +187,8 @@ main (int argc, char ** argv)
 	zprivs_init (&ospfd_privs);
 	master = thread_master_create ();
 
+	lsdb = ospf_lsdb_new ();
+
 	while ((ch = getopt (argc, argv, "s:")) != -1) {
 		switch (ch) {
 		case 's' :
@@ -127,13 +201,13 @@ main (int argc, char ** argv)
 	}
 
 	if (!apisrv) {
-		printf ("-s api server must be specified\n");
+		D ("-s api server must be specified\n");
 		return 1;
 	}
 
 	oc = ospf_apiclient_connect (apisrv, ASYNCPORT);
 	if (!oc) {
-		printf ("Connecting to OSPF daemon of %s failed!\n", apisrv);
+		D ("Connecting to OSPF daemon of %s failed!\n", apisrv);
 		exit (1);
 	}
 	
@@ -144,7 +218,7 @@ main (int argc, char ** argv)
 	ospf_apiclient_sync_lsdb (oc);
 
 	/* schedule thread that handles asynchronous messages */
-	thread_add_read (master, lsa_read, oc, oc->fd_async);
+	thread_add_read (master, iplbospfd_lsa_read, oc, oc->fd_async);
 
 	while (1) {
 		thread_fetch (master, &thread);
